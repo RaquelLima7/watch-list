364b5baaec0e0d51266e06785b4c38b850867022
Raquel Lima <raqueljones182@gmail.com>
2021-04-24 17:06:21 -0300

    finish MCV lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,27 +1,43 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @lists = List.all
   end
 
-  def show
-    @list = List.find(params[:id])
-  end
+  def show; end
 
   def new
     @list = List.new
   end
 
   def create
-    @list = List.new(restaurant_params)
+    @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path
+      redirect_to list_path(@list)
     else
       render "new"
     end
   end
 
+  def edit; end
+
+  def update
+    @list.update(list_params)
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
-  def restaurant_params
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
     params.require(:list).permit(:name)
   end
 end
